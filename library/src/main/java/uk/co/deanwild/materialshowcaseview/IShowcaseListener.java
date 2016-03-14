3aa0d6fad709f7230cc26bfbbb6bbd2cf95ac3ba
makzimi <makzimi@gmail.com>
2016-03-14 12:45:09 +0300

    Update IShowcaseListener.java

--- library/src/main/java/uk/co/deanwild/materialshowcaseview/IShowcaseListener.java
+++ library/src/main/java/uk/co/deanwild/materialshowcaseview/IShowcaseListener.java
@@ -5,4 +5,5 @@ public interface IShowcaseListener {
     void onShowcaseDisplayed(MaterialShowcaseView showcaseView);
     void onShowcaseDismissed(MaterialShowcaseView showcaseView);
     void onShowcaseButtonClick(MaterialShowcaseView showcaseView);
+    void onShowcaseTapIntoShape(MaterialShowcaseView showcaseView);
 }
